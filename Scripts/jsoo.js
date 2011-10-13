75ab8e7a9824dff003901e839e7fcc35798180f8
Tymoteusz Paszun <tpaszun@gmail.com>
2011-10-13 14:36:14 +0300

    added Jsoo licence

--- Scripts/jsoo.js
+++ Scripts/jsoo.js
@@ -1,24 +1,24 @@
-﻿/* UserSketch Class & Events Prototype Implementation
- * Author: Dariusz Dziuk
+/* Jsoo v0.1
  *
- * TODO : Better multiple inheritance
- *
- * Usage:
- * var MyClass = new Class( { firstProperty: 1, secondProperty: 2, method: function() { } } ) ;
- * var MyClass = new Class( { initialize: function() { // contents of constructor } } ) ;
- * var MySubClass = new Class( { Extends: MyClass } ) ; 
- *
- * Using Events:
- * - Adding event listener:
- *	 someObject.addEventListener( "eventName", function( sender, event ) ) ;
- *   someObject.addEventListener( "eventName", { _data_array_ }, function( sender, event )
- *
- *   You can access from event object:
- *   event.params (event parameters)
- *   event.data (event handler's data) if set 
- *
- * - Firing the event:
- *	 someObject.fireEvent( "eventName", params ) ;
+ * Copyright (C) 2011 by Dariusz Dziuk
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 /* Class implementation */
