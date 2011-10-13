92241d0731344ab60ebab65680197d90c1adc676
Tymoteusz Paszun <tpaszun@gmail.com>
2011-10-14 01:05:32 +0200

    test changes

--- Tests/jslogo.test.builtin.math.js
+++ Tests/jslogo.test.builtin.math.js
@@ -55,9 +55,9 @@ test( "sum", function() {
 	raises( function() { funExecutor.execute('sum', []) }, 'INVALID_ARGUMENT_COUNT', "sum shall not be able to be called with 0 inputs" );
 	raises( function() { funExecutor.execute('sum', [one]) }, 'INVALID_ARGUMENT_COUNT', "sum shall not be able to be called with 1 number input" );
 	raises( function() { funExecutor.execute('sum', [lorem]) }, 'INVALID_ARGUMENT_COUNT', "sum shall not be able to be called with 1 word input" );
-	raises( function() { funExecutor.execute('sum', [lorem, ipsum]) }, 'INVALID_ARGUMENT_COUNT', "sum shall not be able to be called with 2 word inputs" );
-	raises( function() { funExecutor.execute('sum', [lorem, one]) }, 'INVALID_ARGUMENT_COUNT', "sum shall not be able to be called with 1 word and 1 number input" );
-	raises( function() { funExecutor.execute('sum', [one, lorem]) } , 'INVALID_ARGUMENT_COUNT', "sum shall not be able to be called with 1 number and 1 word input" );
+	raises( function() { funExecutor.execute('sum', [lorem, ipsum]) }, 'INVALID_INPUT_TYPE', "sum shall not be able to be called with 2 word inputs" );
+	raises( function() { funExecutor.execute('sum', [lorem, one]) }, 'INVALID_INPUT_TYPE', "sum shall not be able to be called with 1 word and 1 number input" );
+	raises( function() { funExecutor.execute('sum', [one, lorem]) } , 'INVALID_INPUT_TYPE', "sum shall not be able to be called with 1 number and 1 word input" );
 
 	deepEqual( funExecutor.execute('sum', [one, two]), three, "sum of 1 and 2 should be 3" );
 	deepEqual( funExecutor.execute('sum', [m_one, m_two]), m_three, "sum of -1 and -2 should be -3" );
